508707c8249c49cd750cc48268392e9c243d9986
mukund <mukundagrawal1234@gmail.com>
2020-07-20 17:52:28 -0700

    Updates

---
+++ precache-manifest.9e5a2a46277f4654e09ddb3eee3405e2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a421b095caf521d79c462146ba5d04",
+    "url": "/My-First-React-Website/index.html"
+  },
+  {
+    "revision": "9d300e3513aef475af96",
+    "url": "/My-First-React-Website/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "f7d479b446d20e7fe2f0",
+    "url": "/My-First-React-Website/static/css/main.09eb91db.chunk.css"
+  },
+  {
+    "revision": "9d300e3513aef475af96",
+    "url": "/My-First-React-Website/static/js/2.bbce93c9.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/My-First-React-Website/static/js/2.bbce93c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7d479b446d20e7fe2f0",
+    "url": "/My-First-React-Website/static/js/main.78f7280c.chunk.js"
+  },
+  {
+    "revision": "d8ded8a3d9b973068630",
+    "url": "/My-First-React-Website/static/js/runtime-main.5f9704e7.js"
+  },
+  {
+    "revision": "b012bb7398009f3af941a14bb60c90e4",
+    "url": "/My-First-React-Website/static/media/app.b012bb73.jpg"
+  },
+  {
+    "revision": "a94328262e3049edd2c9454db56de6f9",
+    "url": "/My-First-React-Website/static/media/web.a9432826.jpg"
+  }
+]);
